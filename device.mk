27f7794aa8c0bf7f9fe7ab539a0329a202ff3d0a
Mithil Aggarwal <mithil.aggrawal@gmail.com>
2019-07-08 05:06:24 +0000

    Remove QTI Telephony

--- device.mk
+++ device.mk
@@ -237,7 +237,7 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
-    ims-ext-common
+   qti-telephony-common
 
 # Input
 PRODUCT_COPY_FILES += \
@@ -365,8 +365,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service
 
 # Telephony
-PRODUCT_PACKAGES += \
-    telephony-ext
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
